677e79782b9e5928fb234c4a66cfb8defd248d64
dhiraares <mybasmalah123@gmail.com>
2025-05-13 20:42:27 -0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,57 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EsmrlDYCjN":
+        Script1();
+        break;
+      case "6EnY2JrmLxe":
+        Script2();
+        break;
+      case "69qxfdbEpE2":
+        Script3();
+        break;
+      case "5fIJNNJf3s5":
+        Script4();
+        break;
+      case "6XQdxu0tymD":
+        Script5();
+        break;
+      case "5pbVtPMADPn":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  parent.playMediaMusic();
+
+}
+
+function Script2()
+{
+  parent.toggleMute();
+}
+
+function Script3()
+{
+  parent.toggleMute();
+}
+
+function Script4()
+{
+  parent.playGameMusic();
+
+}
+
+function Script5()
+{
+  parent.toggleMute();
+}
+
+function Script6()
+{
+  parent.endGame();
+}
+
